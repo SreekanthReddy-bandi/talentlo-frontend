c515a33b2e7be37ab2a2ef49d4ee63f65347030e
SreekanthReddy-bandi <sreekanth.reddy@talentlo.com>
2025-01-25 14:39:38 +0530

    Test
    Test commit

--- src/components/Launching.tsx
+++ src/components/Launching.tsx
@@ -95,7 +95,7 @@ const Launching: React.FC = () => {
                 </span>
               </label>
             </div>
-            <button className="w-full bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-6">
+            <button className="w-full rounded mt-6">
               Apply as a Freelancer
             </button>
             <p className="mt-4 text-center text-black">Already have an account? <a href="#" className="text-blue-600">Log in</a></p>
